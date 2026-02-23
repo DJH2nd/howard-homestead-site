23b30b449e2a6e25572058bddf48d2818355a2b4
DJH2nd <djhoward2nd@gmail.com>
2026-02-23 00:35:12 -0500

    Delete site.js

--- site.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  const path = (location.pathname.split('/').pop() || 'index.html').toLowerCase();
-  document.querySelectorAll('.navlinks a').forEach(a=>{
-    const href = (a.getAttribute('href')||'').toLowerCase();
-    if(href === path) a.classList.add('active');
-  });
-})();
